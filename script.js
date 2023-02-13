f70ce3361e466bb028c997b441b79870b78ce2df
Esraa-Adel99 <61186023+Esraa-Adel99@users.noreply.github.com>
2023-02-13 14:29:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function openHome()
+{
+	open(URL="file:///C:/Users/dell/Desktop/Clinc/clinic.html");
+   
+}
+
+function log()
+{
+	open(URL="file:///C:/Users/dell/Desktop/Clinc/index%20(4).html");
+}
